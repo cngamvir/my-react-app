aa15828b15dd6a9731f7c52e512b8c826d9dd9d1
Chanya Ngamvirojcharoen <c.ngamvirojcharoen@wustl.edu>
2026-03-30 21:29:06 -0500

    Defined new state variable (currentMove).

--- 7d5727f0-81a2-47e5-87f5-5ed68d5e99b5/src/App.js
+++ 7d5727f0-81a2-47e5-87f5-5ed68d5e99b5/src/App.js
@@ -55,6 +55,7 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
   const currentSquares = history[history.length - 1];
 
   function handlePlay(nextSquares) {
@@ -62,7 +63,7 @@ export default function Game() {
     setXIsNext(!xIsNext);
   }
 
-    function jumpTo(nextMove) {
+  function jumpTo(nextMove) {
     // TODO
   }
 
@@ -86,7 +87,7 @@ export default function Game() {
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-         <ol>{moves}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
@@ -111,3 +112,4 @@ function calculateWinner(squares) {
   }
   return null;
 }
+
